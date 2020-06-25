7483ff60e48b98864ec1a1dc7822b260eda9680b
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-06-24 22:15:36 -0400

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -19,13 +19,13 @@ function App() {
       </div>
       <div className="footer">
         <footer>
-          <p>Tanks for
+          <p>Thanks for
              <a href="https://www.youtube.com/watch?v=16Pg7RzTB7U&list=PLSLSMRLK7Yfi-bM_Xq0VWpoqygKRvgRdW">Masood Sadri</a>
             ---
             Codes is Available in <a target="_blank" href="https://github.com/thisisjasonjafari/ShopBasketFrontCast">Github</a>
 
             ---
-            You can Watch the  <a target="_blank" href="https://thisisjasonjafari.github.io/ShopBasketFrontCast/">Demo</a>
+            You can Watch the  <a target="_blank" href="https://thisisjasonjafari.github.io/ShopBasketFrontCast/">Demo</a> here
           </p>
 
         </footer>
