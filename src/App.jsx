ee3e99229f89f28d999d47a2b90c22d3096e1ca0
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-06-24 22:11:15 -0400

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,20 @@ function App() {
         </header>
         <ProductList />
         <Cart />
+
+      </div>
+      <div className="footer">
+        <footer>
+          <p>Tanks for
+             <a href="https://www.youtube.com/watch?v=16Pg7RzTB7U&list=PLSLSMRLK7Yfi-bM_Xq0VWpoqygKRvgRdW">Masood Sadri</a>
+            ---
+            Codes is Available in <a target="_blank" href="https://github.com/thisisjasonjafari/ShopBasketFrontCast">Github</a>
+
+            ---
+            You can Watch the  <a target="_blank" href="https://thisisjasonjafari.github.io/ShopBasketFrontCast/">Demo</a>
+          </p>
+
+        </footer>
       </div>
     </CartProvider>
   );
