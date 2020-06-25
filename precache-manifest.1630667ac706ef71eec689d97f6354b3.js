c0aafc6bdc23021041f62cc2f43efbed5de8211e
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-06-24 22:02:40 -0400

    Updates

---
+++ precache-manifest.1630667ac706ef71eec689d97f6354b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6a192e844c51c6ee56a13480f8caa03",
+    "url": "/ShopBasketFrontCast/index.html"
+  },
+  {
+    "revision": "7fb7b7bb33f8e4cb03e6",
+    "url": "/ShopBasketFrontCast/static/css/main.45387041.chunk.css"
+  },
+  {
+    "revision": "73e58c7f84fea2aeba0a",
+    "url": "/ShopBasketFrontCast/static/js/2.5dd43186.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ShopBasketFrontCast/static/js/2.5dd43186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fb7b7bb33f8e4cb03e6",
+    "url": "/ShopBasketFrontCast/static/js/main.cd4be334.chunk.js"
+  },
+  {
+    "revision": "6975b9e0d1c1f2619802",
+    "url": "/ShopBasketFrontCast/static/js/runtime-main.794902fb.js"
+  }
+]);
